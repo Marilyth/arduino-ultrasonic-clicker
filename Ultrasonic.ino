f16a3ad68a93bb356f6eb3b345779cbb9c924c8d
May <baermay98@gmail.com>
2022-10-12 18:04:23 +0200

    Set range on startup

--- Ultrasonic.ino
+++ Ultrasonic.ino
@@ -1,11 +1,20 @@
 #include <HCSR04.h>
 #include <Wire.h>
 
-UltraSonicDistanceSensor distanceSensor(2, 3, 400);  // Initialize sensor that uses trigger pin 2 and echo pin 3.
+UltraSonicDistanceSensor distanceSensor(2, 3, 400);  // Initialize sensor that uses digital pins 13 and 12.
 int buzzerPin = 12;
+float startingDistanceCm = -1;
 
 void setup() {
   pinMode(buzzerPin, OUTPUT);
+
+  delay(1000);
+  
+  double value = distanceSensor.measureDistanceCm();
+  startingDistanceCm = value > 0 ? value : 400;
+
+  // Cut 2% to avoid inaccuracies to click.
+  startingDistanceCm *= 0.98;
 }
 
 void loop() {
@@ -13,8 +22,10 @@ void loop() {
   if(value < 0)
     value = 400;
 
-  double closeness = 1.0 - (value / 400);
-  if(closeness > 0.65)
+  double closeness = 1.0 - (value / startingDistanceCm);
+  // If further away than startingDistance, set to 0.
+  closeness = closeness < 0 ? 0 : closeness;
+  if(closeness > 0)
     click(closeness);
   
   delay(50);
